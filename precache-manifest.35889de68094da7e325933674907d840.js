6d3388a2e06066bc8db540c7e75f5b81ccab53d2
Tom-M-Git <astral.taney@gmail.com>
2020-08-24 22:17:16 +0900

    Updates

---
+++ precache-manifest.35889de68094da7e325933674907d840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8cb8cc80bbf7a6943c34bef0261bdf1",
+    "url": "/projectmanager_with_react/index.html"
+  },
+  {
+    "revision": "44354b84435c4a29b5d1",
+    "url": "/projectmanager_with_react/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "09a8ef730e16817ae8ec",
+    "url": "/projectmanager_with_react/static/js/2.2ff4ae19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectmanager_with_react/static/js/2.2ff4ae19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44354b84435c4a29b5d1",
+    "url": "/projectmanager_with_react/static/js/main.03cf68bd.chunk.js"
+  },
+  {
+    "revision": "9881f20950a229cfd8e9",
+    "url": "/projectmanager_with_react/static/js/runtime-main.8a87d334.js"
+  }
+]);
