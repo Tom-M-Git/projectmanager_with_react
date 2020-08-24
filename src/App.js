bb9496ea76891a77d71f679a077c09b8101840a5
Tom-M-Git <astral.taney@gmail.com>
2020-08-24 21:15:55 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
+import Projects from './components/Projects';
+import AddProject from './components/AddProject';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: []
+    }
+  }
+
+  componentWillMount(){
+    this.setState({
+      projects: [
+        {
+          id: uuidv4(),
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          id: uuidv4(),
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          id: uuidv4(),
+          title: 'Ecommerce Shopping Cart',
+          category: 'Web Development'
+        }
+      ]
+    });
+  }
+
+  handleAddProject(project){
+    let projects = this.state.projects;
+    projects.push(project);
+    this.setState({projects:projects});
+  }
+
+  handleDeleteProject(id){
+    let projects = this.state.projects;
+    let index = projects.findIndex(x => x.id === id);
+    projects.splice(index, 1);
+    this.setState({projects:projects});
+  }
+
+  render() {
+      return (
+        <div className="App">
+          <AddProject addProject={this.handleAddProject.bind(this)} />
+          <Projects projects={this.state.projects} onDelete={this.handleDeleteProject.bind(this)} />
+        </div>
+      );
+  }
 }
 
 export default App;
